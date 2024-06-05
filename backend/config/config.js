46c964e32d6763ce66a4225aec5e291478ecb8eb
sheheryarghaznavi <sheheryarghaznavi@yahoo.com>
2024-06-06 02:37:25 +0500

    frontend uri added at backend

--- backend/config/config.js
+++ backend/config/config.js
@@ -5,6 +5,7 @@ module.exports = {
         clientId: process.env.OUTLOOK_CLIENT_ID,
         clientSecret: process.env.OUTLOOK_CLIENT_SECRET,
         redirectUri: process.env.OUTLOOK_REDIRECT_URI,
+        frontendRedirectUri: process.env.FRONTEND_REDIRECT_URI,
         scopes: 'openid profile offline_access User.Read Mail.Read',
     }
 };
